fae6a4d728765ce590d268f482ffbf252c7948fd
abhiram-ar <abhiram2001.ars@gmail.com>
2024-11-21 20:05:49 +0530

    login route

--- server/Readme.md
+++ server/Readme.md
@@ -1,5 +1,10 @@
- 
-
- 
 ### MongoDB connection string pattern
 mongodb+srv://<db_username>:<db_password>@cluster0.s3euj.mongodb.net/<dbname>?retryWrites=true&w=majority&appName=Cluster0
+
+
+### Users Schema
+- email: { type: String, index: true, unique: true }
+- username: { type: String, required: true }
+- password: { type: String, required: true }
+- isAdmin: { type: Boolean, default: false }
+- createdAt: { type: Date, default: Date.now() }
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,16 +1,53 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const UserModel = require("./../models/userModal");
+const { z, string } = require("zod");
 
 const router = express.Router();
+const userAuthSchema = z.object({
+    username: z
+        .string()
+        .min(5, { message: "username should contain 5 or more characters" })
+        .optional(),
+    email: z.string().email({ message: "Invalid email address" }),
 
-router.get("/signup", (req, res) => {
+    password: z
+        .string()
+        .min(8, { message: "password should contain atlest 8 characters" }),
+    isAdmin: z.boolean().optional(),
+});
+
+router.post("/signup", async (req, res) => {
     const { username, email, password, isAdmin } = req.body;
-    console.log(req.body);
 
     //validate credentials and reject if invalid
-    
+    const user = userAuthSchema.safeParse({
+        username,
+        email,
+        password,
+        isAdmin,
+    });
+    if (!user.success) {
+        console.log("invalid user credentials");
+        return res.status(400).json(user.error);
+    }
 
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({
+        username,
+        email,
+        password: hashedpassword,
+        isAdmin,
+    });
+    try {
+        await newUser.save();
+        console.log("user saved sucessful");
+    } catch (error) {
+        console.log(`saving user failed`);
+        return res.status(400).json({ success: "failed" });
+    }
 
-    res.send("singup");
+    res.status(201).json({ success: true });
 });
 
 module.exports = router;
