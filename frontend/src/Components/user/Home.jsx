e2e58f8ca131ef458652512fe17e46c953f7a20f
abhiram-ar <abhiram2001.ars@gmail.com>
2024-11-28 21:56:41 +0530

    user page ui

--- frontend/src/Components/user/Home.jsx
+++ frontend/src/Components/user/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
         try {
             const response = await logout().unwrap();
             console.log("logoutResponse", response);
-            
+
             dispatch(clearStore());
             navigate("/login", { replace: true });
         } catch (error) {
@@ -53,18 +53,36 @@ const Home = () => {
 
     return (
         <div>
-            <button onClick={handleLogout}>logout</button>
-            <h1>hello {isSuccess && data.username}</h1>
-            <div>
-                <img src={`${isSuccess ? data.profilePicURL : ""}`} alt="" />
-            </div>
-            <input
-                type="file"
-                onChange={(e) => setSelectedImage(e.target.files[0])}
-            />
-            <button onClick={handleProfilePicUpdate} disabled={isLoading}>
-                Update
+            <button
+                onClick={handleLogout}
+                className="absolute right-16 top-5 px-3 py-2 rounded-2xl font-semibold border border-black  bg-[#8B8B8B] hover:bg-red-500"
+            >
+                Logout
             </button>
+
+            <h1 className="text-2xl bg-[#F2FED1] w-fit font-bold p-2 mt ms-16 mt-5">
+                hello {isSuccess && data.username}
+            </h1>
+            <div className="m-auto w-fit flex flex-col gap-10 justify-center items-center mt-20 border border-black p-10 rounded-lg bg-zinc-200">
+                <div className="size-44 overflow-hidden border rounded-xl border-black shadow-[0px_10px_0px_-2px_rgba(0,0,0,1)]">
+                    <img
+                        src={`${isSuccess ? data.profilePicURL : ""}`}
+                        alt=""
+                    />
+                </div>
+                <input
+                    type="file"
+                    onChange={(e) => setSelectedImage(e.target.files[0])}
+                    className="bg-[rgb(237,244,254)]  px-3 py-1 rounded-2xl border border-black hover:bg-blue-400"
+                />
+                <button
+                    onClick={handleProfilePicUpdate}
+                    disabled={isLoading}
+                    className=" bg-[#F2FED1] border  border-black px-3 py-1 rounded-2xl hover:bg-[#ABF600]"
+                >
+                    Update
+                </button>
+            </div>
         </div>
     );
 };
